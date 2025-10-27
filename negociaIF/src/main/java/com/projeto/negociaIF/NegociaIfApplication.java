488b859ca87ee4c5d8d011fc0481e95517e39797
Elisa Ranikelly <elisaranikelly2004@gmail.com>
2025-10-27 16:55:33 -0300

    organizei as questões do banco de dados

---
+++ negociaIF/src/main/java/com/projeto/negociaIF/NegociaIfApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.negociaIF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegociaIfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegociaIfApplication.class, args);
+	}
+
+}
---
+++ negociaIF/src/test/java/com/projeto/negociaIF/NegociaIfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.negociaIF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegociaIfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
